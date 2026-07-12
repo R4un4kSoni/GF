b2e5594011b47c37306a6424768c4c820ff067ea
R4un4kSoni <r4un4k20@gmail.com>
2026-07-12 14:24:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+name = 'xyz'
+print(name)
+
+user_input = input("Send a message: ")
+time.sleep(1)
+print("\n\n\n message seen \n ")
+time.sleep(2)
+print("Typing.... \n\n\n")
+
+time.sleep(2)
+
+print(f"You have been blocked by {name} \n ")
+print(f"note by {name}: Fuck you, don't you ever text me back")
